d8a97ddcb59fac56307866d8c10a33b8ec7356fd
roglio <leandro.zaccaria@gmail.com>
2015-10-18 13:27:19 +0200

    Some cleanup
    Removed a blocking for loop.

--- Arduino/img2h/img2h.ino
+++ Arduino/img2h/img2h.ino
@@ -17,38 +17,26 @@
 // use this pinout:
 #define CLK 8  // MUST be on PORTB! (Use pin 11 on Mega)
 #define OE  9
-#define LAT 10
+#define LAT 18 //10 
 #define A   A0
 #define B   A1
 #define C   A2
 #define D   A3
-// If your matrix has the DOUBLE HEADER input, use:
-//#define CLK 8  // MUST be on PORTB! (Use pin 11 on Mega)
-//#define LAT 9
-//#define OE  10
-//#define A   A3
-//#define B   A2
-//#define C   A1
-//#define D   A0
+
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false);
 
 void setup() {
   matrix.begin();
+  matrix.fillScreen(matrix.Color333(0,0,0));
 }
 
 void loop() {
-  // fill the screen with 'black'
-  matrix.fillScreen(matrix.Color333(0,0,0));
-  delay(500);
-
   uint8_t *ptr = matrix.backBuffer(); // Get address of matrix data
-
   int frames = sizeof(image)/sizeof(image[0]);
 
-  for(;;) {
-    for(int i=0;i<frames;i++){
-      memcpy_P(ptr, image[i], 1536);
-      delay(100);
-    }
+  for(int i=0;i<frames;i++){
+    memcpy_P(ptr, image[i], 1536);
+    delay(100);
   }
 }
+
